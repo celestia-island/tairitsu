9f4b05e7b9cdff22f5413d419c0d44bdbc335b00
伊欧 <langyo.china@gmail.com>
2024-04-20 09:37:01 +0800

    ⚗️ Try to send the request to the remote.

--- examples/guest-side/src/main.rs
+++ examples/guest-side/src/main.rs
@@ -5,6 +5,9 @@ use anyhow::Result;
 use wasi::sockets::{
     instance_network::instance_network,
     ip_name_lookup::{resolve_addresses, IpAddress},
+    network::{IpAddressFamily, Ipv4SocketAddress},
+    tcp::IpSocketAddress,
+    tcp_create_socket::create_tcp_socket,
 };
 
 use tairitsu_utils::types::proto::backend::Msg;
@@ -30,25 +33,56 @@ async fn main() -> Result<()> {
         "{}",
         serde_json::to_string(&Msg::new("debug", "Try to send ip lookup")).unwrap()
     );
-    let request = resolve_addresses(&instance_network(), "cor-games.com")?;
-    loop {
+    let request = resolve_addresses(&instance_network(), "localhost")?;
+    let ip = loop {
         if let Ok(ret) = request.resolve_next_address() {
-            let ret = ret
-                .map(|addr| match addr {
-                    IpAddress::Ipv4((a, b, c, d)) => format!("{}.{}.{}.{}", a, b, c, d),
-                    IpAddress::Ipv6((a, b, c, d, e, f, g, h)) => format!(
-                        "{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}",
-                        a, b, c, d, e, f, g, h
-                    ),
-                })
-                .unwrap_or("Unknown address".to_string());
             println!(
                 "{}",
-                serde_json::to_string(&Msg::new("debug", ret)).unwrap()
+                serde_json::to_string(&Msg::new("debug", format!("IP: {:?}", ret))).unwrap()
             );
+
+            match ret {
+                Some(IpAddress::Ipv4(ip)) => break ip,
+                _ => unimplemented!("Only IPv4 is supported"),
+            }
+        }
+    };
+    let request = create_tcp_socket(IpAddressFamily::Ipv4)?;
+    loop {
+        if request
+            .start_connect(
+                &instance_network(),
+                IpSocketAddress::Ipv4(Ipv4SocketAddress {
+                    address: ip,
+                    port: 8080,
+                }),
+            )
+            .is_ok()
+        {
             break;
         }
     }
+    let (input, output) = loop {
+        if let Ok(ret) = request.finish_connect() {
+            break ret;
+        }
+    };
+    output.write(b"GET / HTTP/1.1\r\n")?;
+    output.write(b"Host: localhost\r\n")?;
+    output.write(b"\r\n")?;
+    output.flush()?;
+    let response = loop {
+        // FIXME - Only read 100000 bytes at first
+        let response = input.read(100000)?;
+        if response.len() > 0 && response[0] != 0 {
+            break response;
+        }
+    };
+    let response = String::from_utf8_lossy(&response).to_string();
+    println!(
+        "{}",
+        serde_json::to_string(&Msg::new("debug", format!("Response: {:?}", response))).unwrap()
+    );
 
     Ok(())
 }
