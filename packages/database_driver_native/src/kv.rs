d2669ad159b442c33d762d7e530e1805c6aa75e4
伊欧 <langyo.china@gmail.com>
2024-08-26 17:24:38 +0800

    ✨ Complete `kv` store for native target.

--- packages/database_driver_native/src/kv.rs
+++ packages/database_driver_native/src/kv.rs
@@ -1,39 +1,61 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+
+use sled::Db;
 
 use tairitsu_database_types::providers::kv::*;
 
 #[derive(Clone)]
 pub struct ProxyKV {
-    #[allow(dead_code)]
-    path: String,
+    db: Db,
 }
 
 #[async_trait::async_trait]
 impl KVStore for ProxyKV {
-    async fn set(&self, _key: String, _value: String) -> Result<()> {
-        todo!()
+    async fn set(&self, key: String, value: String) -> Result<()> {
+        self.db.insert(key.into_bytes(), value.into_bytes())?;
+
+        Ok(())
     }
 
-    async fn get(&self, _key: String) -> Result<Option<String>> {
-        todo!()
+    async fn get(&self, key: String) -> Result<Option<String>> {
+        let value = self.db.get(key.into_bytes())?;
+
+        if let Some(value) = value {
+            Ok(Some(String::from_utf8(value.to_vec())?))
+        } else {
+            Ok(None)
+        }
     }
 
-    async fn delete(&self, _key: String) -> Result<()> {
-        todo!()
+    async fn delete(&self, key: String) -> Result<()> {
+        self.db.remove(key.into_bytes())?;
+
+        Ok(())
     }
 
     async fn list_by_prefix(
         &self,
-        _prefix: String,
-        _limit: Option<usize>,
-        _cursor: Option<String>,
+        prefix: String,
+        limit: Option<usize>,
+        cursor: Option<String>,
     ) -> Result<Vec<String>> {
-        todo!()
+        let ret = self
+            .db
+            .scan_prefix(prefix.as_bytes())
+            .keys()
+            .skip(cursor.map_or(0, |cursor| cursor.parse().unwrap_or(0)))
+            .take(limit.unwrap_or(std::usize::MAX))
+            .map(|key| {
+                key.map(|key| String::from_utf8(key.to_vec()).unwrap_or_default())
+                    .map_err(|err| anyhow!("{}", err))
+            })
+            .collect::<Vec<_>>();
+        ret.into_iter().collect::<Result<Vec<_>>>()
     }
 }
 
 pub async fn init_kv(path: impl ToString) -> Result<ProxyKV> {
     Ok(ProxyKV {
-        path: path.to_string(),
+        db: sled::open(path.to_string())?,
     })
 }
